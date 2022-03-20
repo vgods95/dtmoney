d8d8f6572aabce8dd6e545dc560e65a88860f23a
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2022-03-20 11:02:34 -0300

    Avança na implementação da criação de uma nova transação

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ export function App() {
   function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false);
   }
+  
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
